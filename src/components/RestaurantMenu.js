a80a677e572d45c06db51815cf2a10f7fb85bc5a
Abhishek <abhishekbhikule76@gmail.com>
2024-01-30 20:46:20 +0530

    Continue with same debugging

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -23,8 +23,8 @@ const RestaurantMenu = () => {
   const { resId } = useParams();
 
   // API endpoints for desktop and mobile
-  const regularMenuEndpoint = `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D19.0177989%26lng%3D72.84781199999999%26restaurantId=${resId}`;
-  const mobileMenuEndpoint = `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D19.0177989%26lng%3D72.84781199999999%26restaurantId=${resId}`;
+  const regularMenuEndpoint = `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D19.0177989%26lng%3D72.84781199999999%26restaurantId%3D${resId}%26catalog_qa%3Dundefined%26submitAction%3DENTER`;
+  const mobileMenuEndpoint = `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D19.0177989%26lng%3D72.84781199999999%26restaurantId%3D${resId}%26isMenuUx4%3Dtrue%26submitAction%3DENTER`;
 
   // Fetch data based on the device
   const resInfo = useRestaurant(
